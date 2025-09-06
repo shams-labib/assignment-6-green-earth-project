557b3cbbe64e4a4b9316b8d1e800e988342d4730
shams al labib <samslabib19@gmail.com>
2025-09-07 03:05:42 +0600

    modal function added

--- js/index.js
+++ js/index.js
@@ -8,6 +8,31 @@ const category = () => {
     })
 }
 
+// modal show koarnor jonno
+const modalDataLoad = (id) => {
+    const url = `https://openapi.programming-hero.com/api/plant/${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        displayModal(data.plants);
+    })
+}
+const displayModal = (id) => {
+       const container = document.getElementById("word-container");
+       container.innerHTML = ` 
+       <div class="space-y-3 p-4">
+            <h1 class="font-semibold text-xl">${id.name}</h1>
+            <img class="h-[40vh] w-full rounded-lg" src="${id.image}" alt="">
+            <p class="font-semibold">Category : ${id.category}</p>
+            <p>Price: ${id.price}</p>
+            <h3>Description : ${id.description}</h3>
+        </div>
+       `
+
+       document.getElementById("my_modal_5").showModal();
+}
+
+
 const removeActive = ()=> {
    const remove = document.querySelectorAll('.activeBtn')
    remove.forEach(rm => {
@@ -34,7 +59,7 @@ const displaAllplants = (posts)=> {
                     <h1 class = 'font-semibold'>${post.name}</h1>
                     <p>${post.description}</p>
                     <div class="flex justify-between">
-                        <button class="btn  rounded-3xl bg-[#DCFCE7]">${post.category}</button>
+                        <button onclick="modalDataLoad(${post.id})" class="btn  rounded-3xl bg-[#DCFCE7]">${post.category}</button>
                         <h3><span class = "text-2xl">৳</span>${post.price}</h3>
                     </div>
                     <button class="btn bg-[#15803D] text-white w-full rounded-3xl">Add to Cart</button>
@@ -69,7 +94,7 @@ const displaLoadData = (data) => {
                     <h1 class = 'font-semibold'>${post.name}</h1>
                     <p>${post.description}</p>
                     <div class="flex justify-between">
-                        <button class="btn  rounded-3xl bg-[#DCFCE7]">${post.category}</button>
+                        <button onclick="modalDataLoad(${post.id})" class="btn  rounded-3xl bg-[#DCFCE7]">${post.category}</button>
                         <h3><span class = "text-2xl">৳</span>${post.price}</h3>
                     </div>
                     <button class="btn bg-[#15803D] text-white w-full rounded-3xl">Add to Cart</button>
